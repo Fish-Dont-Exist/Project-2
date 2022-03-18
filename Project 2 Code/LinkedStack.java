d266a5962ead17578871216d644625268e3d1565
Benjamin Dinh <benjadinh@gmail.com>
2022-03-17 20:40:53 -0700

    Removing redundant code

--- Project 2 Code/LinkedStack.java
+++ Project 2 Code/LinkedStack.java
@@ -42,64 +42,6 @@ public class LinkedStack<T> implements StackInterface<T>
 		topNode = null;
 	}// end clear
 
-
-	public String convertToPostFix(String infix)
-	{
-		int characterCount = infix.length();
-		int index = 0;
-		char nextCharacter = ' ';
-		char topOperator = ' ';
-		LinkedStack<Character> operatorStack = new LinkedStack<>();
-		String postfix = "";
-
-		while (index < characterCount) // while there are characters left in the string
-		{
-			nextCharacter = infix.charAt(index); // get character at index
-			switch(nextCharacter) // check what type the character is
-			{
-				case 'a': case 'b': case 'c': case 'd': //if it is a variable add it to postfix
-					postfix += infix.charAt(index);
-					break;
-
-				case '^':
-					operatorStack.push(nextCharacter); // adds ^ to stack
-					break;
-
-				case '+': case '-': case '*': case '/':
-					while(!operatorStack.isEmpty()) //&& precedenc of next character <= precendence of operatorStack.peek()) // if there is something inside the operator stack
-					{
-						postfix += operatorStack.peek(); // add the top item of the stack
-						operatorStack.pop(); // remove top item of the stack
-					}
-					operatorStack.push(nextCharacter); // add character to stack
-					break;
-				case '(':
-					operatorStack.push(nextCharacter); // add character to stack
-					break;
-				case ')': 
-					topOperator = operatorStack.peek(); // sets topOperator to top stack item 	
-					operatorStack.pop(); // removes top of the stack
-					while (topOperator != '(')
-					{
-						postfix += topOperator; // add topOperator to postfix
-						topOperator = operatorStack.peek(); // add top of stack to topOperator
-						operatorStack.pop(); // remove top of stack
-					}
-					break;
-				default: break;
-			}
-			index++;
-		}
-		while (!operatorStack.isEmpty())
-		{
-			topOperator = operatorStack.peek();
-			operatorStack.pop();
-			postfix += topOperator;
-		}
-
-		return postfix;
-	}
-
 	private class Node
    {
       private T    data; //Entry in the bag
